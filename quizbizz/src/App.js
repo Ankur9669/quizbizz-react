f6c57e15e266d69e3d3f2989805566442f1ded40
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-10 20:31:39 +0530

    initial commit

---
+++ quizbizz/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">This is Ankur</header>
+    </div>
+  );
+}
+
+export default App;
