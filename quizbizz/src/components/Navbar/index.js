8787ed9553c06f2e2da17455a6cb9bcc6f5e2519
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-13 11:44:03 +0530

    changed the name of folders

--- quizbizz/src/components/Navbar/index.js
+++
@@ -1,6 +0,0 @@
-import Avatar from "../../assets/images/male-avatar.svg";
-import { AiOutlineMenu } from "../../assets/icons/icons";
-import QuizBizz from "../../assets/images/quiz-bizz.svg";
-import PrimaryButton from "../buttons/PrimaryButton";
-
-export { Avatar, AiOutlineMenu, QuizBizz, PrimaryButton };
