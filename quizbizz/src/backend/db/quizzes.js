698fef307d939647bcb1fea43b9a147d07130079
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-18 15:39:33 +0530

    added backend code for getting single quiz

---
+++ quizbizz/src/backend/db/quizzes.js
@@ -0,0 +1,64 @@
+export const quizzes = [
+  {
+    _id: "1",
+    quizName: "React",
+    quizImage:
+      "https://github.com/Ankur9669/quizbizz-react/blob/dev/quizbizz/src/assets/images/css.png",
+    questions: [
+      {
+        questionNumber: 1,
+        question: "What is React",
+        options: [
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+        ],
+      },
+      {
+        questionNumber: 2,
+        question: "What is React",
+        options: [
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+        ],
+      },
+      {
+        questionNumber: 3,
+        question: "What is React",
+        options: [
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+        ],
+      },
+      {
+        questionNumber: 4,
+        question: "What is React",
+        options: [
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+        ],
+      },
+      {
+        questionNumber: 5,
+        question: "What is React",
+        options: [
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+          "It is awesome",
+        ],
+      },
+    ],
+    answers: ["a", "b", "c", "d"],
+  },
+];
+// https://github.com/Ankur9669/quizbizz-react/blob/dev/quizbizz/src/assets/images/java.jpeg
+// https://github.com/Ankur9669/quizbizz-react/blob/dev/quizbizz/src/assets/images/javascript.png
+// https://github.com/Ankur9669/quizbizz-react/blob/dev/quizbizz/src/assets/images/react.png
--- quizbizz/src/server.js
+++ quizbizz/src/server.js
@@ -4,40 +4,15 @@ import {
   signupHandler,
 } from "./backend/controllers/AuthController";
 import {
-  getHistoryVideosHandler,
-  addVideoToHistoryHandler,
-  removeVideoFromHistoryHandler,
-  clearHistoryHandler,
-} from "./backend/controllers/HistoryController";
-import {
   getAllVideosHandler,
   getVideoHandler,
 } from "./backend/controllers/VideoController";
+import { getQuizById } from "./backend/controllers/QuizController";
 import { videos } from "./backend/db/videos";
 import { categories } from "./backend/db/categories";
-import {
-  getAllCategoriesHandler,
-  getCategoryHandler,
-} from "./backend/controllers/CategoryController";
-import {
-  getLikedVideosHandler,
-  addItemToLikedVideos,
-  removeItemFromLikedVideos,
-} from "./backend/controllers/LikeController";
-import {
-  getAllPlaylistsHandler,
-  addNewPlaylistHandler,
-  removePlaylistHandler,
-  getVideosFromPlaylistHandler,
-  addVideoToPlaylistHandler,
-  removeVideoFromPlaylistHandler,
-} from "./backend/controllers/PlaylistController";
 import { users } from "./backend/db/users";
-import {
-  addItemToWatchLaterVideos,
-  getWatchLaterVideosHandler,
-  removeItemFromWatchLaterVideos,
-} from "./backend/controllers/WatchLaterController";
+import { quizzes } from "./backend/db/quizzes";
+
 export function makeServer({ environment = "development" } = {}) {
   return new Server({
     serializers: {
@@ -53,6 +28,7 @@ export function makeServer({ environment = "development" } = {}) {
       history: Model,
       playlist: Model,
       watchlater: Model,
+      quizzes: Model,
     },
 
     // Runs on the start of the server
@@ -60,6 +36,9 @@ export function makeServer({ environment = "development" } = {}) {
       server.logging = false;
       videos.forEach((item) => {
         server.create("video", { ...item });
+      });
+      quizzes.forEach((item) => {
+        server.create("quiz", { ...item });
       });
       categories.forEach((item) => server.create("category", { ...item }));
       users.forEach((item) =>
@@ -83,54 +62,7 @@ export function makeServer({ environment = "development" } = {}) {
       this.get("/videos", getAllVideosHandler.bind(this));
       this.get("video/:videoId", getVideoHandler.bind(this));
 
-      // TODO: POST VIDEO TO DB
-
-      // categories routes (public)
-      this.get("/categories", getAllCategoriesHandler.bind(this));
-      this.get("/categories/:categoryId", getCategoryHandler.bind(this));
-
-      // likes routes (private)
-      this.get("/user/likes", getLikedVideosHandler.bind(this));
-      this.post("/user/likes", addItemToLikedVideos.bind(this));
-      this.delete("/user/likes/:videoId", removeItemFromLikedVideos.bind(this));
-
-      // watch later routes (private)
-      this.get("/user/watchlater", getWatchLaterVideosHandler.bind(this));
-      this.post("/user/watchlater", addItemToWatchLaterVideos.bind(this));
-      this.delete(
-        "/user/watchlater/:videoId",
-        removeItemFromWatchLaterVideos.bind(this)
-      );
-
-      // playlist routes (private)
-      this.get("/user/playlists", getAllPlaylistsHandler.bind(this));
-      this.post("/user/playlists", addNewPlaylistHandler.bind(this));
-      this.delete(
-        "/user/playlists/:playlistId",
-        removePlaylistHandler.bind(this)
-      );
-
-      this.get(
-        "/user/playlists/:playlistId",
-        getVideosFromPlaylistHandler.bind(this)
-      );
-      this.post(
-        "/user/playlists/:playlistId",
-        addVideoToPlaylistHandler.bind(this)
-      );
-      this.delete(
-        "/user/playlists/:playlistId/:videoId",
-        removeVideoFromPlaylistHandler.bind(this)
-      );
-
-      // history routes (private)
-      this.get("/user/history", getHistoryVideosHandler.bind(this));
-      this.post("/user/history", addVideoToHistoryHandler.bind(this));
-      this.delete(
-        "/user/history/:videoId",
-        removeVideoFromHistoryHandler.bind(this)
-      );
-      this.delete("/user/history/all", clearHistoryHandler.bind(this));
+      this.get("quiz/:quizId", getQuizById.bind(this));
     },
   });
 }
