85a17bbe2a1d044d27215c3c933a10266ab3b093
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2022-04-18 16:08:03 +0530

    cleaned up the files that were not required

--- quizbizz/src/server.js
+++ quizbizz/src/server.js
@@ -3,13 +3,8 @@ import {
   loginHandler,
   signupHandler,
 } from "./backend/controllers/AuthController";
-import {
-  getAllVideosHandler,
-  getVideoHandler,
-} from "./backend/controllers/VideoController";
+
 import { getQuizById } from "./backend/controllers/QuizController";
-import { videos } from "./backend/db/videos";
-import { categories } from "./backend/db/categories";
 import { users } from "./backend/db/users";
 import { quizzes } from "./backend/db/quizzes";
 
@@ -21,33 +16,21 @@ export function makeServer({ environment = "development" } = {}) {
     environment,
     // TODO: Use Relationships to have named relational Data
     models: {
-      video: Model,
-      category: Model,
       user: Model,
-      like: Model,
-      history: Model,
-      playlist: Model,
-      watchlater: Model,
       quizzes: Model,
     },
 
     // Runs on the start of the server
     seeds(server) {
       server.logging = false;
-      videos.forEach((item) => {
-        server.create("video", { ...item });
-      });
+
       quizzes.forEach((item) => {
         server.create("quiz", { ...item });
       });
-      categories.forEach((item) => server.create("category", { ...item }));
+
       users.forEach((item) =>
         server.create("user", {
           ...item,
-          likes: [],
-          watchlater: [],
-          history: [],
-          playlists: [],
         })
       );
     },
@@ -58,10 +41,7 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/auth/signup", signupHandler.bind(this));
       this.post("/auth/login", loginHandler.bind(this));
 
-      // video routes (public)
-      this.get("/videos", getAllVideosHandler.bind(this));
-      this.get("video/:videoId", getVideoHandler.bind(this));
-
+      // Quiz Routes
       this.get("quiz/:quizId", getQuizById.bind(this));
     },
   });
